d92ff557eaf12e3bd0a2bf500602ce6e5b48929c
Carlos Badenes <cbadenes@gmail.com>
2019-11-17 11:29:54 -0800

    fixed command

--- steps/2-create-topics-en.sh
+++ steps/2-create-topics-en.sh
@@ -1,1 +1,1 @@
-curl --user "demo:2019" -X POST -H "Content-Type: application/json" -d @../json/topic-model—en.json http://localhost:8081/topics
+curl --user "demo:2019" -X POST -H "Content-Type: application/json" -d @../json/topic-model-en.json http://localhost:8081/topics
